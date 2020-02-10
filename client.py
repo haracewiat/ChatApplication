d112c11e344708afa6502d26c34e7323635fd2cb
venoivankovic <35835812+venoivankovic@users.noreply.github.com>
2020-02-10 04:35:25 +0100

    Add files via upload
    quit works (just had to set daemon to true in thread)
    login does not work, but should be a quick fix

--- client.py
+++ client.py
@@ -10,57 +10,57 @@ BUFFER = 4096
 
 class Client:
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    #thread_receive = threading.Thread
+    thread_list = []
+    socket_closed = False
+    login_correct = False
 
     def __init__(self):
         self.connect()
-
         self.login()
-
-        # send & receive
-
-        #thread_send = threading.Thread(target=self.send)
-        #thread_send.start()
-
-        thread_receive = threading.Thread(target=self.receive)
+        thread_receive = threading.Thread(target=self.receive,  daemon=True)
         thread_receive.start()
-
-        self.wait_for_command()
-
-        #thread_wait_for_command = threading.Thread(target=self.wait_for_command)
-        #thread_wait_for_command.start()
-
-        # disconnect
-        self.disconnect()
-
-    def send(self):
-        while True:
-            string_bytes = sys.stdin.readline().encode("utf-8")
-            self.sock.send(string_bytes)
+        if not self.socket_closed:
+            self.wait_for_command()
+        else:
+            sys.exit()
 
     def receive(self):
         while True:
             data = self.sock.recv(BUFFER)
             if not data:
                 print("Socket is closed.")
-                #time.sleep(1)
-                #self.wait_for_command()
+                self.socket_closed = True
+                break
                 # FIXME if user exists, print is executed endlessly
             else:
                 print(data.decode("utf-8"))
-                #time.sleep(1)
-                #self.wait_for_command()
-                # time.sleep(10)
+        sys.exit()
+
+    def send_as_bytes(self, send_this):
+        send_this_as_bytes = send_this.encode("utf-8")
+        self.sock.send(send_this_as_bytes)
 
     def login(self):
         print("First, let's log in. Type in your name:")
         name = input()
+        if name == "!quit":
+            self.quit_command(name)
         send_login = "HELLO-FROM " + name + "\n"
-        send_login_as_bytes = send_login.encode("utf-8")
-        self.sock.send(send_login_as_bytes)
+        self.send_as_bytes(send_login)
+        #login_response_as_bytes = self.sock.recv(BUFFER)
+        #login_response = login_response_as_bytes.decode("utf-8")
+        #print(login_response)
+        #if login_response == "IN-USE\n":
+         #   print(login_response)
+          #  print("Try another name: ")
+           # newname = input()
+            #if newname == "quit":
+             #   self.quit_command()
+            #send_login = "HELLO-FROM " + name + "\n"
+            #self.send_as_bytes(send_login)
+
 
     def wait_for_command(self):
-        #print("Type a command:")
         command = input()
         if command == "!quit":
             self.quit_command(command)
@@ -70,23 +70,17 @@ class Client:
             self.send_command(command[1:])
 
     def who_command(self, command):
-        #print("who command here")
         send_who = "WHO\n"
-        send_who_as_bytes = send_who.encode("utf-8")
-        self.sock.send(send_who_as_bytes)
+        self.send_as_bytes(send_who)
         self.wait_for_command()
-        # self.receive()
 
     def quit_command(self, command):
-        print("QUIT")
         self.disconnect()
-        sys.exit()
 
     def send_command(self, command):
         name, msg = command.split(' ', 1)
         send_msg = "SEND " + name + " " + msg + "\n"
-        send_msg_as_bytes = send_msg.encode("utf-8")
-        self.sock.send(send_msg_as_bytes)
+        self.send_as_bytes(send_msg)
         self.wait_for_command()
 
     def connect(self):
@@ -95,11 +89,11 @@ class Client:
         except:
             print('Cannot establish connection. Aborting.')
             sys.exit()
-
         print('Connected to remote host.')
 
     def disconnect(self):
         self.sock.close()
+        print("Closed socket. Exiting...")
         sys.exit()
 
 
